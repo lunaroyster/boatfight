132a19821fbc8eff48c5c01c3d6ece26e5a49db0
lunaroyster <oneinaquadrillion@live.com>
2016-08-23 19:01:03 +0530

    Cell properties

--- grid.py
+++ grid.py
@@ -1,6 +1,10 @@
 class cell(object):
     """docstring for cell"""
+
+    color = 'def'
+    occupied = False
+    hit = False
+
     def __init__(self, arg):
         super(cell, self).__init__()
-        self.arg =x arg
-    
+        self.arg = x
