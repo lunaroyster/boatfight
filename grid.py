3512892f32f7a1e04e60a45d0ddf568b61b92083
lunaroyster <oneinaquadrillion@live.com>
2016-08-23 19:01:17 +0530

    Grid

--- grid.py
+++ grid.py
@@ -1,3 +1,17 @@
+class grid(object):
+    """docstring for grid"""
+    # Cell = [][]
+    def __init__(self, w, h):
+        super(grid, self).__init__()
+        self.w = w
+        self.h = h
+        # Cell = [[0 for x in range(w)] for y in range(h)]
+        Cell = [[0 for x in range(w)] for x in range(h)]
+        for i in range(h):
+            for j in range(w):
+                Cell[i][j] = cell
+        print(Cell)
+
 class cell(object):
     """docstring for cell"""
 
